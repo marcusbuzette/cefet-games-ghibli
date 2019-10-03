8229f83a76f3af714b72301d54befef66b6b69c5
Aluno do CEFET <aluno@cefetmg.br>
2019-10-03 12:20:51 -0300

    first commit

--- core/assets/shaders/fragment.glsl
+++ core/assets/shaders/fragment.glsl
@@ -14,7 +14,7 @@ void main()
 {
     // DEBUG: esta cor pode ser usada para "depurar". Pinte o fragmento com ela,
     // que está azulzinha
-    vec4 debugColor = vec4(0.5, 0.5, 1.0, 1.0);
+    vec4 debugColor = vec4(0, 0, 0, 0);
 
     // DEBUG: use este if para mudar a cor para vermelho caso 
     // "algo esteja errado". No final, defina o gl_FragColor como esta cor...
@@ -26,20 +26,22 @@ void main()
     }
 
     // para calcular a componente difusa, precisamos:
-    // vec3 normal = ...
-    // vec3 incidenciaLuz = ...
+    vec3 normal = normalize(v_fragNormal);
+        
+    vec3 incidenciaLuz = normalize(u_dirLights0direction) * -1.0;
 
     // para calcular a componente especular, precisamos:
-    // vec3 visualizacao = ...
-    // vec3 reflexao = ...
+     vec3 visualizacao = normalize(v_fragPosition);
+     vec3 reflexao = normalize(u_dirLights0direction );
 
     // calcula as 3 componentes de Phong: ambiente, difusa, especular
-    // vec4 ambiente = ...
-    // vec4 difusa = ...
-    // vec4 especular = ...
+    vec4 ambiente = vec4(0.25,0.25,0.25,0.25);
+    vec4 difusa = max(dot(normal, incidenciaLuz), 0.0) * u_dirLights0color * 
+        texture2D(u_diffuseTexture, v_texCoords);
+    vec4 especular = max(0.0, dot(reflexao, visualizacao)) * u_dirLights0color;
 
     // Dá o resultado
-    gl_FragColor = vec4(v_fragNormal, 1.0);
+    gl_FragColor =  ambiente + difusa + especular ;
 
     // DEBUG: para depurar, use a linha a seguir para definir a cor do fragmento
     //gl_FragColor = debugColor;
